364a57f904e151affd388bc0909996adb88b944b
josh <bugabu@live.com.mx>
2019-09-04 19:06:26 -0500

    se guardo el index

---
+++ Index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <title>Proyecto Clinica</title>
+  </head>
+  <body>
+    <nav class="navbar sticky-top navbar-dark bg-dark navbar-expand-md">
+      <a class="navbar-brand" href="index.php">Clinica</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a href="#" class="nav-link">xd</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Medicos</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Cirujanos</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Clientes</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Graficas</a></li>
+        </ul>
+      </div>
+    </nav>
+    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <table class="table table-dark table-bordered">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>nombre</th>
+                <th>edad</th>
+                <th>xd</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>sd</th>
+                <th>fgdg</th>
+                <th>gfhfg</th>
+                <th>hgfh</th>
+              </tr>
+              <tr>
+                <th>sd</th>
+                <th>fgdg</th>
+                <th>gfhfg</th>
+                <th>hgfh</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <br />
+        <div class="col-sm-8">
+          <div class="card border-dark mb-3">
+            <div class="card-header">
+              <h2>Formulario</h2>
+            </div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-group">
+                  <label for="text">Nombre</label>
+                  <input
+                    type="text"
+                    id="xd"
+                    class="form-control"
+                    placeholder="Nombre"
+                  />
+                  <br />
+                  <button type="reset" class="btn btn-dark">limpiar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row"></div>
+    </div>
+    <div class="container fixed-bottom bg-dark text-white mw-100">
+        <h4>Joshua Stefano Velazquez Sanchez</h4>
+    </div>
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
